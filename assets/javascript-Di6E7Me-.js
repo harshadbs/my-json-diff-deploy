1ed77827896b260a86fea1121b2b08340bd90fb7
harshad_bs <harshad>
2025-09-01 22:28:18 +0530

    new update with extra full screen

--- assets/javascript-Di6E7Me-.js
+++ assets/javascript-Di6E7Me-.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-CjRE0a4i.js";import"./index-DDlWSo0b.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-DbcXFFtJ.js";import"./index-GphsSmKC.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
